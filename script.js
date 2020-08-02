632007f15a9175b732c6c563e18fcebe723999eb
Deonte Archie <deontearchie@icloud.com>
2020-08-02 11:28:27 -0400

    I made my number generator work but My prompt is not connected to the generator but i still got the generator to generate how many characters i would like

--- script.js
+++ script.js
@@ -3,31 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-    let passwordLength = 8
+        
+    // let passwordLength = document.getElementById("slider").value;
 
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*_+"
-    return values[Math.floor(Math.random() * values.length)]
-    var string = ""
-    retval = "";
+        
+    let passwordLength = prompt("Enter the length of your password: (Must be between 8 and 128 Char)")
 
-
-
-    for (var i = 0; i < passwordLength; i++) {
-        retval = password + values.charAt(Math.floor(Math.random() * value.length))
-        console.log(password)
+    if (confirm("Would you like a Uppercase Character in your password?")) {
 
     }
-    return retval;
+    if (confirm("Would you like a lowercase Character in your password?")) {
 
+    }
+    if (confirm("Would you like a Number in your password?")) {
 
+    }
+    if (confirm("Would you like a special Character in your password?")) {
+
+    }
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*_+"    
+        // let valuesUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ23456789";    
+        // let valuesLower = "abcdefghijklmnopqrstuvwxyz";    
+        // let valuesNum = "1234567890";    
+        // let valuesSpec = "!@#$%^&*_+";    
+
+        
+    let password = "";
+
+        
+    for (i = 0; i <= passwordLength; i++) { 
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));    
+    }    
+    alert(password)
 }
 
 // Write password to the #password input
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-
+function writePassword() {    
+    var password = generatePassword();    
+    var passwordText = document.querySelector("#password");    
+    passwordText.value = password;    
 }
 
 // Add event listener to generate button
